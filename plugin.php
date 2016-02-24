786781e40f294e0b5cb2163d6815dd6e5cad82d0
Arya Wiratama <arya@maxsolution.co.id>
2016-02-24 16:08:03 +0800

    form handler

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@ Domain Path:
 add_action('init', function() {
 	include dirname( __FILE__ ).'/includes/class-ziparea-admin-menu.php';
 	include dirname( __FILE__ ).'/includes/class-zip-list-table.php';
-	include dirname( __FILE__ ).'/includes/class-form-handler.php';
+	include dirname( __FILE__ ).'/includes/class-form-handler-ziparea.php';
 	include dirname( __FILE__ ).'/includes/zip-functions.php';
 	
 	new Ziparea_Admin_Menu();
