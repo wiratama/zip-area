18a5bd7677ea65abb54fca95416db0e2a309ceee
Arya Wiratama <arya@maxsolution.co.id>
2016-02-24 16:03:16 +0800

    update

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Zip Area
-Plugin URI: 
+Plugin URI: https://github.com/wiratama/zip-area
 Description: Zip area palyja
 Version: 0.1
 Author: Arya Wiratama
@@ -11,22 +11,11 @@ Text Domain:
 Domain Path: 
 */
 
-/*add_action( 'init', 'sambungan_baru_palyja' );
-
-function sambungan_baru_palyja() {
-	include dirname( __FILE__ ).'/includes/class-sambunganbaru-admin-menu.php';
-	include dirname( __FILE__ ).'/includes/class-sambunganbaru-list-table.php';
-	include dirname( __FILE__ ).'/includes/class-form-handler.php';
-	include dirname( __FILE__ ).'/includes/sambunganbaru-functions.php';
-	
-	new Sambunganbaru_Admin_Menu();
-}*/
-
 add_action('init', function() {
 	include dirname( __FILE__ ).'/includes/class-ziparea-admin-menu.php';
-	/*include dirname( __FILE__ ).'/includes/class-sambungan-list-table.php';
+	include dirname( __FILE__ ).'/includes/class-zip-list-table.php';
 	include dirname( __FILE__ ).'/includes/class-form-handler.php';
-	include dirname( __FILE__ ).'/includes/sambungan-functions.php';*/
+	include dirname( __FILE__ ).'/includes/zip-functions.php';
 	
 	new Ziparea_Admin_Menu();
 });
